77894389f114e5ffc2f2d9879dc5cd16b1b93457
duganum <61757804+duganum@users.noreply.github.com>
2026-01-23 19:16:43 -0600

    Update render_v2_GitHub.py

--- render_v2_GitHub.py
+++ render_v2_GitHub.py
@@ -6,7 +6,7 @@ def render_problem_diagram(prob_id):
     pid = str(prob_id).strip()
     
     # Initialize the small figure
-    fig, ax = plt.subplots(figsize=(5, 4), dpi=100)
+    fig, ax = plt.subplots(figsize=(5, 4), dpi=300)
     ax.set_aspect('equal')
     found = False
 
